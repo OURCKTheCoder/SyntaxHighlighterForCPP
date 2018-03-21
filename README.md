447647f7e3938742db16a8f712c17ea8d041aa25
ourck <ourck@LENOVO-OURCK>
2018-03-21 20:51:08 +0800

    [+] Receive 2 args as input file & output

--- README.md
+++ README.md
@@ -1,4 +1,1 @@
 # SyntaxHighlighterForCPP
-A c++ syntax highlighter witten by Java.
-There's a sample CPP files in SyntaxHighlighterForCPP/ for testing.
-Using NDF Automata.(穷举法不是万能的...)
--- src/ourck/mainconsole/MainConsole.java
+++ src/ourck/mainconsole/MainConsole.java
@@ -2,12 +2,13 @@ package ourck.mainconsole;
 
 import ourck.tokens.*;
 import ourck.textfile.*;
+import java.io.*;
 
 public class MainConsole {
 	public static void main(String[] args) {
 		try {
-//			FileAsText txtFile = new FileAsText(args[0]); // TODO Path here.
-			FileAsText txtFile = new FileAsText("main.cpp"); // TODO Debug path here.
+			FileAsText txtFile = new FileAsText(args[0]); // TODO Path here.
+//			FileAsText txtFile = new FileAsText("main.cpp"); // TODO Debug path here.
 			
 			LTGTMaker lgMgr = new LTGTMaker();
 //			NbspMaker nbMgr = new NbspMaker();
@@ -24,42 +25,56 @@ public class MainConsole {
 			
 			// ---------------- The show begins! ----------------
 			String[] lines = txtFile.toStrAryByLines();
-			StringBuilder newToken = new StringBuilder();
+			StringBuilder newFile = new StringBuilder();
 			for(String line : lines) {
-				newToken.append(line);
+				newFile.append(line);
 			}
 			
-			lgMgr.analyze(newToken);
-			cmMgr.analyze(newToken); // [!] Analyze in advance for these method:
-//			nbMgr.analyze(newToken);
-			strMgr.analyze(newToken);
-			csMgr.analyze(newToken);
+			lgMgr.analyze(newFile);
+			cmMgr.analyze(newFile); // [!] Analyze in advance for these method:
+//			nbMgr.analyze(newFile);
+			strMgr.analyze(newFile);
+			csMgr.analyze(newFile);
 			
-//			brMgr.analyze(newToken);
-//			tbMgr.analyze(newToken);
+//			brMgr.analyze(newFile);
+//			tbMgr.analyze(newFile);
 			
-			cmMgr.analyze(newToken); cmMgr.highlightAll(newToken);
-			ppMgr.analyze(newToken);
-			kwMgr.analyze(newToken);
+			cmMgr.analyze(newFile); cmMgr.highlightAll(newFile);
+			ppMgr.analyze(newFile);
+			kwMgr.analyze(newFile);
 			
-			char[] formatted = newToken.toString().toCharArray();
+			char[] formatted = newFile.toString().toCharArray();
 			int baseindex = 0;
 			for(int i = 0; i < formatted.length; i++) {
 				if(formatted[i] == '\n') {
-					newToken.insert(i + baseindex, "</pre><pre>");
+					newFile.insert(i + baseindex, "</pre><pre>");
 					baseindex += "</pre><pre>".length();
 				}
 			}
 			
-			newToken.insert(0, "<font size=\"4\">\n" + "<pre>\n");
-			newToken.append('\n' + "</pre>\n</font>"); // 早知道有pre标签还换个头的格式...
+			newFile.insert(0, "<font size=\"4\">\n" + "<pre>\n");
+			newFile.append('\n' + "</pre>\n</font>"); // 早知道有pre标签还换个头的格式...
 			
-			System.out.println(newToken);
+			System.out.println("-----------------OUTPUT-----------------");
+			System.out.println(newFile);
+			System.out.println("--------------=== ENDS ===--------------");
+			
+			
+			BufferedWriter out;
+			if(args.length == 2) 
+				out = new BufferedWriter(new FileWriter(
+						new File(args[1]).getAbsoluteFile()));
+			else 
+				throw new ArrayIndexOutOfBoundsException("PLZ give valid input path & output path!");
+			out.write(newFile.toString());
+			out.close();
 			
 		} catch(NullPointerException e) {
 			System.err.println(" [!] Open file failed!");
 		} catch (ArrayIndexOutOfBoundsException e) {
-			System.err.println(" [!] PLZ give a valid path!");
+			System.err.println(" [!] Ary out of bounds! \n" + e.getMessage());
+		} catch (IOException e) {
+			System.err.println(" [!] Saving failed! ");
 		} catch(RuntimeException e) {
 			System.err.println(" [!] Runtime failed: " + e.getCause().toString());
 		} catch (Exception e) {
